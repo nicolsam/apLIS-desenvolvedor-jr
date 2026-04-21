3752b6e214950d541084f8d4fb30882a704b39fc
Nícolas Samuel <nicolas.lorrayne@gmail.com>
2026-04-21 18:18:48 -0300

    chore: add Tailwind CSS and Flowbite to frontend

---
+++ app/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'class'
+}
